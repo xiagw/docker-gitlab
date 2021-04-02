cd320d9cf5c44963a3623ec929ecb1b1610af062
xiagw <fxiaxiaoyu@gmail.com>
2021-04-02 21:28:11 +0800

    add auto.sh,change port 23

---
+++ auto.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+## 修改sshd_config
+# sudo sed -i -e '/^#Port\ 22/s//Port 23/' /etc/ssh/sshd_config
+# sudo systemctl restart sshd
+
+## 生成自签名证书
+if [ ! -f .env ]; then
+    cp env-example .env
+    read -r -p "Enter your domain name: " domain_name
+    sed -i -e "s/example.com/${domain_name:?empty var}/g" .env
+fi
+
+source .env
+
+mkdir -p gitlab/config
+
+openssl genrsa -out "gitlab/config/${DOMAIN_NAME:?empty var}.key" 2048
+openssl req -new -key "gitlab/config/${DOMAIN_NAME}.key" -out "gitlab/config/${DOMAIN_NAME}.csr" -subj "/CN=default/O=default/C=UK"
+openssl x509 -req -days 3650 -in "gitlab/config/${DOMAIN_NAME}.csr" -signkey "gitlab/config/${DOMAIN_NAME}.key" -out "gitlab/config/${DOMAIN_NAME}.crt"
+chmod 644 "gitlab/config/${DOMAIN_NAME}.key"
+
+## 启动gitlab server
+docker-compose up -d gitlab
