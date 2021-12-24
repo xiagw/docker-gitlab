da7c7788f24cab1195071e6269aa1258bacdd9f8
xiagw <fxiaxiaoyu@gmail.com>
2021-12-24 22:21:53 +0800

    start gitlab http

--- auto.sh
+++ auto.sh
@@ -18,29 +18,43 @@ if [ ! -f .env ]; then
 fi
 
 ## acme.sh generate ssh key
-read -rp "Do you want to generate ssh key for acme.sh? [y/N] " -e -i 'N' yn
-if [[ ${yn:-N} == [yY] ]]; then
-    echo "Generate ssh key for acme.sh deployment"
-    if [[ ! -f acmeout/id_ed25519 ]]; then
-        [[ -d acmeout ]] || mkdir acmeout
-        ssh-keygen -t ed25519 -N '' -f acmeout/id_ed25519
-    fi
-    unset yn
-fi
+# read -rp "Do you want to generate ssh key for acme.sh? [y/N] " -e -i 'N' yn
+# if [[ ${yn:-N} == [yY] ]]; then
+#     echo "Generate ssh key for acme.sh deployment"
+#     if [[ ! -f acmeout/id_ed25519 ]]; then
+#         [[ -d acmeout ]] || mkdir acmeout
+#         ssh-keygen -t ed25519 -N '' -f acmeout/id_ed25519
+#     fi
+#     unset yn
+# fi
 
 source .env
 ## 生成自签名证书，（不建议）
 dir_ssl='gitlab/config/ssl'
-if grep '^GITLAB_LETSENCRYPT=false' .env && [ ! -f "${dir_ssl}/${DOMAIN_NAME_GIT}.key" ]; then
-    read -rp "Do you want to generate self-signed certificate? [y/N] " -e -i 'N' yn
-    if [[ ${yn:-N} == [yY] ]]; then
-        [ -d "${dir_ssl}" ] || mkdir -p "${dir_ssl}"
-        openssl genrsa -out "${dir_ssl}/${DOMAIN_NAME_GIT:?empty var}.key" 2048
-        openssl req -new -key "${dir_ssl}/${DOMAIN_NAME_GIT}.key" -out "${dir_ssl}/${DOMAIN_NAME_GIT}.csr" -subj "/CN=${DOMAIN_NAME_GIT}/O=${DOMAIN_NAME_GIT}/C=HK"
-        openssl x509 -req -days 3650 -in "${dir_ssl}/${DOMAIN_NAME_GIT}.csr" -signkey "${dir_ssl}/${DOMAIN_NAME_GIT}.key" -out "${dir_ssl}/${DOMAIN_NAME_GIT}.crt"
-        chmod 644 "${dir_ssl}/${DOMAIN_NAME_GIT}.key"
+[ -d "${dir_ssl}" ] || mkdir -p "${dir_ssl}"
+# read -rp "Do you want to use https? [y/N] " -e -i 'N' yn
+if [[ -f "${DOMAIN_NAME_GIT}.key" && -f "${DOMAIN_NAME_GIT}.crt" ]]; then
+    echo "Found ${DOMAIN_NAME_GIT}.key ${DOMAIN_NAME_GIT}.crt"
+    cp -v "${DOMAIN_NAME_GIT}.key" "${DOMAIN_NAME_GIT}.crt" "${dir_ssl}/"
+else
+    echo "Not found ${DOMAIN_NAME_GIT}.key ${DOMAIN_NAME_GIT}.crt"
+    if grep '^GITLAB_LETSENCRYPT=false' .env && [ ! -f "${dir_ssl}/${DOMAIN_NAME_GIT}.key" ]; then
+        read -rp "Do you want to generate self-signed certificate? [y/N] " -e -i 'N' yn
+        if [[ ${yn:-N} == [yY] ]]; then
+            openssl genrsa -out "${dir_ssl}/${DOMAIN_NAME_GIT:?empty var}.key" 2048
+            openssl req -new -key "${dir_ssl}/${DOMAIN_NAME_GIT}.key" -out "${dir_ssl}/${DOMAIN_NAME_GIT}.csr" -subj "/CN=${DOMAIN_NAME_GIT}/O=${DOMAIN_NAME_GIT}/C=HK"
+            openssl x509 -req -days 3650 -in "${dir_ssl}/${DOMAIN_NAME_GIT}.csr" -signkey "${dir_ssl}/${DOMAIN_NAME_GIT}.key" -out "${dir_ssl}/${DOMAIN_NAME_GIT}.crt"
+            chmod 644 "${dir_ssl}/${DOMAIN_NAME_GIT}.key"
+        fi
     fi
 fi
 
 ## 启动gitlab server
-docker-compose up -d gitlab watchtower
+read -rp "Do you want to start gitlab server? [y/N] " -e -i 'N' yn
+if [[ ${yn:-N} == [yY] ]]; then
+    echo "Start gitlab server"
+    docker-compose up -d gitlab watchtower
+    unset yn
+fi
+# docker-compose exec -T gitlab-runner gitlab-runner register --non-interactive --url "http://${DOMAIN_NAME_GIT}" --registration-token "${GITLAB_RUNNER_TOKEN}" --executor "docker" --docker-image "gitlab/gitlab-runner:latest" --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" --docker-privileged
+# docker-compose exec -T gitlab-runner gitlab-runner run --user root --working-directory /home/git --env GITLAB_CI_RUNNER_DESCRIPTION="Gitlab Runner" --env GITLAB_CI_RUNNER_TAGS="docker,linux" --env GITLAB_CI_RUNNER_ACCESS_TOKEN="${GITLAB_RUNNER_TOKEN}" --env GITLAB_CI_RUNNER_EXECUTOR="docker" --env GITLAB_CI_RUNNER_DOCKER_IMAGE="gitlab/gitlab-runner:latest" --env GITLAB_CI_RUNNER_DOCKER_VOLUMES="/var/run/docker.sock:/var/run/docker.sock" --env GITLAB_CI_RUNNER_DOCKER_PRIVILEGED="true" --env GITLAB_CI_RUNNER_DOCKER_NETWORK="gitlab_gitlab-runner" gitlab/gitlab-runner
