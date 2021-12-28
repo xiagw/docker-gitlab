d09f9810f676da068f15fc01302ed884015cdbff
xiagw <fxiaxiaoyu@gmail.com>
2021-12-28 16:35:20 +0800

    fix auto.sh

--- auto.sh
+++ auto.sh
@@ -80,17 +80,17 @@ if [[ ${yn:-N} == [yY] ]]; then
     # sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 
     # Install and run as service
-    # sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
-    sudo gitlab-runner install --user="$USER" --working-directory="$HOME"/runner
-    sudo gitlab-runner start
+    # sudo /usr/local/bin/gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+    sudo /usr/local/bin/gitlab-runner install --user="$USER" --working-directory="$HOME"/runner
+    sudo /usr/local/bin/gitlab-runner start
 
     git clone https://github.com/xiagw/deploy.sh.git ~/runner
     read -rp "Enter your gitlab-runner token: " reg_token
     # reg_token='xxxxxxxx'
-    # sudo gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor docker --docker-image gitlab/gitlab-runner:latest --docker-volumes /var/run/docker.sock:/var/run/docker.sock --docker-privileged
+    # sudo /usr/local/bin/gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor docker --docker-image gitlab/gitlab-runner:latest --docker-volumes /var/run/docker.sock:/var/run/docker.sock --docker-privileged
     sudo /usr/local/bin/gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor shell --tag-list docker,linux --run-untagged --locked --access-level=not_protected
 
     ## create git project
-    # gitlab projcet create --name "pms"
-    # gitlab projcet create --name "devops"
+    # gitlab project create --name "pms"
+    # gitlab project create --name "devops"
 fi
