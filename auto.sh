fef662d9cbabf8712bf297c6fd36a02e04d16fe7
xiagw <fxiaxiaoyu@gmail.com>
2022-06-08 10:49:01 +0800

    fix auto.sh

--- auto.sh
+++ auto.sh
@@ -94,12 +94,16 @@ if [[ ${yn:-N} == [yY] ]]; then
     sudo /usr/local/bin/gitlab-runner start
 
     git clone https://github.com/xiagw/deploy.sh.git "$HOME"/runner
+    ## gitlab-runner register token from gitlab-admin-runner: register an instance runner
     read -rp "Enter your gitlab-runner token: " reg_token
     # reg_token='xxxxxxxx'
     # sudo /usr/local/bin/gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor docker --docker-image gitlab/gitlab-runner:latest --docker-volumes /var/run/docker.sock:/var/run/docker.sock --docker-privileged
-    sudo /usr/local/bin/gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor shell --tag-list docker,linux --run-untagged --locked --access-level=not_protected
+    sudo /usr/local/bin/gitlab-runner register --url "$DOMAIN_NAME_GIT_EXT" --registration-token "${reg_token:?empty var}" --executor shell --tag-list docker,linux --run-untagged --locked --access-level=not_protected -n
 
     ## create git project
+    python3 -m pip install --upgrade pip
+    python3 -m pip install --upgrade python-gitlab
+
     # gitlab project create --name "pms"
     # gitlab project create --name "devops"
 fi
